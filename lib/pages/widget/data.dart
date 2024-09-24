ff1c60976e45b38c23b21e3d893320e0d40ce996
Ardy1an <adithardiyanto0@gmail.com>
2024-09-24 23:07:33 +0700

    data gambar untuk carousel slider

---
+++ lib/pages/widget/data.dart
@@ -0,0 +1,20 @@
+class AppData {
+  AppData._();
+
+  static final List<String> innerStyleImages = [
+    'https://images.unsplash.com/photo-1542314831-068cd1dbfeeb?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fGhvdGVsfGVufDB8fDB8fHww',
+    'https://plus.unsplash.com/premium_photo-1682377521625-c656fc1ff3e1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1686782502813-51579b55f6d8?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1682377521724-ececd24a83b4?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1687960116506-f31f84371838?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1687960116497-0dc41e1808a2?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    'https://plus.unsplash.com/premium_photo-1687960116741-d3a1468fdec1?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqg0PcDvTNya2OcoEYLeJdCxjuhn3TzHhEkg&s',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTjTv2Y_WHe2ruknez_emqtHVEXD5D26u5xAw&s',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRV0mF2uvPSZ57Hd8-rbO25bKcKo1sP3qNKRsSJwZr3FkCtj3tUnZsHHEVKek1OpOHt7dM&usqp=CAU',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLt-h9YhUHDhdCs2xU8wWxnxSXJB60b169qZUWMmSAz9GNq29HwDHE1eoyJZsW1qKj8I8&usqp=CAU',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc9NVY1KQFJbiG8NoBcGvC1-EEOOskfIZGIwrW36Vib8ss5LOt1ZdGJoP79uRe3Zht0nY&usqp=CAU',
+    // 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSwlKyoOEwz0cmST_mD5odRspPzJDT8A2cGkEMMJVKRVnL2ddggUaa13cRAC9VemQXXIK8&usqp=CAU',
+  ];
+}
