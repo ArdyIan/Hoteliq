fef7178f2203bff4b40ca3e1badb2b37cd04154a
Ardy1an <adithardiyanto0@gmail.com>
2024-12-03 23:18:59 +0700

    models

---
+++ lib/models/schedule_model.dart
@@ -0,0 +1,13 @@
+class ScheduleModel {
+  DateTime selectedDate;
+  String hotelName;
+  String hotelImage;
+  double pricePerNight;
+
+  ScheduleModel({
+    required this.selectedDate,
+    required this.hotelName,
+    required this.hotelImage,
+    required this.pricePerNight,
+  });
+}
